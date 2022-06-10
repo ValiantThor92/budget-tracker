71a7c384f8b4a53de7bbd5bc866a6e8d398020da
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-06-10 11:34:09 -0700

    add fetch for transactions

--- public/service-worker.js
+++ public/service-worker.js
@@ -37,3 +37,42 @@ self.addEventListener("activate", event => { // activate handler cleans old cach
       .then(() => self.clients.claim())
   );
 });
+
+self.addEventListener("fetch", event => {
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  if (event.request.url.includes("/api/transaction")) {
+    event.respondWith(
+      caches.open(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
+      })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return caches.opent(RUNTIME_CACHE).then(cache => {
+        return fetch(event.request).then(response => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
+});
