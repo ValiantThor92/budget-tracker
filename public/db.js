8d8b7f353218c9432a8e6aadacdfb786884892eb
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-06-10 07:22:58 -0700

    add checkdatabase function and eventlistener

--- public/db.js
+++ public/db.js
@@ -14,7 +14,7 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-  const transaction = db.transaction(["pending"], "readwrite"); // create a transaction to our indexDB
+  const transaction = db.transaction(["pending"], "readwrite"); // create a transaction to indexDB
   const store = transaction.objectStore("pending");             // access indexDB
   store.add(record);                                            // add record
 };
@@ -25,3 +25,28 @@ function saveRecord(record) {
   store.add(record);                                            // add record
 };
 
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite"); // create a transaction to indexDB
+  const store = transaction.objectStore("pending");             // access indexDB
+  const getAll = store.getAll();                                // use getAll() methot to retrieve all records
+  getAll.onsuccess = function() {                               // POST req to bulk add all transactions in indexDB to the database
+    if(getAll.result.length > 0){                               // if results are less than 0 
+      fetch("/api/transaction/bulk", {                          // fetch and post in json format
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          accept: "application/json, text/plain, */*",
+          "content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {                                                   // if successful 
+        const transaction = db.transaction(["pending"], "readwrite"); // create a transaction to indexDB
+        const store = transaction.objectStore("pending");             // access indexDB
+        store.clear();                                                // clear all items
+      });
+    }
+  };
+};
+
+window.addEventListener("online", checkDatabase);                     // event listener for when app comes back online
